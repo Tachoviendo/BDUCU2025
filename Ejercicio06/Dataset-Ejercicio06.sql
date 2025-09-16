74d85765c2b5247e95dfbcf80f40217e82784bde
Tachoviendo <ignacio.3249@gmail.com>
2025-09-15 21:29:24 -0300

    fix scripts and names

--- Ejercicio06/Dataset-Ejercicio06.sql
+++ Ejercicio06/Dataset-Ejercicio06.sql
@@ -1,6 +1,6 @@
-USE practico1;
+\c ejercicio06
 
-INSERT INTO personas(email, nombre, apellido, fecha_nacimiento) VALUES 
+INSERT INTO persona(email, nombre, apellido, fecha_nacimiento) VALUES 
 ('persona1@gmail.com', 'Alejandro', 'Benítez', '2010-10-01'),
 ('persona2@gmail.com', 'Martina', 'Castro', '2009-10-01'),
 ('persona3@gmail.com', 'Daniel', 'Díaz', '2010-12-01'),
@@ -24,7 +24,7 @@ INSERT INTO amigos(email_persona, email_amigo) VALUES
 ('persona8@gmail.com', 'persona6@gmail.com');
 
 
-INSERT INTO tipo_videojuegos(tipo) VALUES 
+INSERT INTO tipos_videojuegos(tipo) VALUES 
 ('cartas'), 
 ('shooter'),
 ('estrategia'), 
@@ -32,7 +32,7 @@ INSERT INTO tipo_videojuegos(tipo) VALUES
 ('rpg');
 
 
-INSERT INTO videojuegos(nombre, dificultad, descripcion, costo, tipo) VALUES 
+INSERT INTO videojuegos(nombre, dificultad, descripcion, costo, tipo_videojuego) VALUES 
 ('Project Zomboid', '3', 'Project Zomboid is the ultimate in zombie survival. Alone or in MP: you loot, build, craft, fight, farm and fish in a struggle to survive',  429, 'rpg'), 
 ('Tower of Fantasy', '2', 'Embark together on your fantasy adventure! Set hundreds of years in the future on the distant planet of Aida, the shared open-world RPG, anime-infused sci-fi adventure', 0, 'rpg'),
 ('Magic: The Gathering Arena', '3', 'Juego digital de cartas coleccionables desarrollado y publicado por Wizards of the Coast', 0, 'cartas'), 
@@ -41,13 +41,13 @@ INSERT INTO videojuegos(nombre, dificultad, descripcion, costo, tipo) VALUES
 INSERT INTO expansiones(nombre, nombre_videojuego, costo) VALUES 
 ('Battlefield 2042 Ultimate Edition', 'Battlefield 2042', 4800);
 
-INSERT INTO compra (email, nombre_videojuego, nombre_expansion, valor, fecha) VALUES
+INSERT INTO compra (email_persona, nombre_videojuego, nombre_expansion, valor, fecha) VALUES
 ('persona1@gmail.com', 'Project Zomboid', NULL, 429, '2024-01-15'),
 ('persona2@gmail.com', 'Tower of Fantasy', NULL, 0, '2024-02-10'),
 ('persona3@gmail.com', 'Battlefield 2042', 'Battlefield 2042 Ultimate Edition', 7400, '2024-03-22');
 
 
-INSERT INTO juega (email, nombre_videojuego, fecha) VALUES
+INSERT INTO juega (email_persona, nombre_videojuego, fecha) VALUES
 ('persona1@gmail.com', 'Project Zomboid', '2024-01-20'),
 ('persona2@gmail.com', 'Tower of Fantasy', '2024-02-15'),
 ('persona3@gmail.com', 'Battlefield 2042', '2024-03-25');
--- Ejercicio06/scripts/ddl.sql
+++ Ejercicio06/scripts/ddl.sql
@@ -9,7 +9,7 @@ CREATE TABLE persona (
     nombre varchar(60),
     apellido varchar(60),
     fecha_nacimiento DATE,
-    password TEXT NOT NULL, 
+    password TEXT
 );
 CREATE TABLE tipos_videojuegos(
     tipo varchar(60) PRIMARY KEY
@@ -18,37 +18,39 @@ CREATE TABLE tipos_videojuegos(
 CREATE TABLE videojuegos(
     nombre varchar(60) PRIMARY KEY,
     dificultad INT CHECK (dificultad >1 and dificultad<6),
-    descripcion varchar(150),
+    descripcion varchar(400),
     costo decimal, 
     tipo_videojuego varchar(60),
     FOREIGN KEY (tipo_videojuego) REFERENCES tipos_videojuegos(tipo)
 );
 
 CREATE TABLE EXPANSIONES(
-    id_expansiones SERIAL PRIMARY KEY,
     nombre varchar(60),
     nombre_videojuego varchar(60),
     costo decimal, 
-    FOREING KEY (nombre_videojuego) REFERENCES videojuegos(nombre)
-    
+    FOREIGN KEY (nombre_videojuego) REFERENCES videojuegos(nombre),
+    PRIMARY KEY (nombre, nombre_videojuego)
 );
 
 CREATE TABLE AMIGOS(
     email_persona varchar(50),
     email_amigo varchar(50),
     FOREIGN KEY (email_persona) REFERENCES persona(email),
+    FOREIGN KEY (email_amigo) REFERENCES persona(email),
     PRIMARY KEY (email_persona, email_amigo)
 );
 
 CREATE TABLE COMPRA (
-    email varchar(50),
+    id_compra SERIAL PRIMARY KEY,
+    email_persona varchar(50),
     nombre_videojuego varchar(60),
     nombre_expansion varchar(60), 
     valor decimal, 
     fecha date, 
-    FOREIGN KEY (nombre_videojuego) REFERENCES videojuegos(nombre),
-    FOREIGN KEY (nombre_expansion) REFERENCES expansiones(nombre)
-);
+    FOREIGN KEY(email_persona) REFERENCES persona(email),
+    FOREIGN KEY (nombre_expansion, nombre_videojuego) REFERENCES expansiones(nombre, nombre_videojuego),
+    FOREIGN KEY (nombre_videojuego) REFERENCES videojuegos(nombre)
+   );
 
 CREATE TABLE juega(
     email_persona varchar(50),
@@ -58,8 +60,3 @@ CREATE TABLE juega(
     FOREIGN KEY (nombre_videojuego) REFERENCES videojuegos(nombre),
     PRIMARY KEY (email_persona, nombre_videojuego, fecha)
 );
-
-
-
-
-
